bb9e91908a06b6e94dc2e3a18321237de5fc9358
Sasha <sasha.lyutova@gmail.com>
2016-03-14 23:40:21 +0100

    BarcodeScanner usage example

--- app/src/main/java/com/liutova/avocare/view/fragment/BarcodeReaderFragment.java
+++ app/src/main/java/com/liutova/avocare/view/fragment/BarcodeReaderFragment.java
@@ -1,13 +1,30 @@
 package com.liutova.avocare.view.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.liutova.avocare.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by Oleksandra Liutova on 13-Mar-16.
  */
 public class BarcodeReaderFragment extends BaseFragment {
+
+    @Bind(R.id.imgview)
+    ImageView myImageView;
+    @Bind(R.id.txtContent)
+    TextView txtView;
 
     public static BarcodeReaderFragment newInstance() {
 
@@ -23,13 +40,30 @@ public class BarcodeReaderFragment extends BaseFragment {
         return R.layout.fragment_barcode_reader;
     }
 
-    /*@OnClick(R.id.process_button)
+
+    @OnClick(R.id.process_button)
     public void onClickProcessBtn(View view) {
-        @Bind(R.id.imgview) ImageView myImageView;
-        //ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        // process image into bitmap
         Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
+                getBaseActivity().getApplicationContext().getResources(),
                 R.drawable.puppy);
         myImageView.setImageBitmap(myBitmap);
-    }*/
+
+        // create barcode detector
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getBaseActivity().getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if (!detector.isOperational()) {
+            txtView.setText("Could not set up the detector!");
+            return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        TextView txtView = (TextView) getBaseActivity().findViewById(R.id.txtContent);
+        txtView.setText(thisCode.rawValue);
+    }
 }
