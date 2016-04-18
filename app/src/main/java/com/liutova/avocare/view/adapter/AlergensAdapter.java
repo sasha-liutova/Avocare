e4a9ca2fc9634512ebfee0024b061702ebded7e2
Sasha <sasha.lyutova@gmail.com>
2016-04-18 22:38:09 +0200

    Alergens layout - unfinished

--- app/src/main/java/com/liutova/avocare/view/adapter/AlergensAdapter.java
+++ app/src/main/java/com/liutova/avocare/view/adapter/AlergensAdapter.java
@@ -1,37 +1,108 @@
 package com.liutova.avocare.view.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 
+import com.liutova.avocare.R;
+
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 /**
  * Created by liutoole on 4/18/16.
  */
-public class AlergensAdapter extends RecyclerView.Adapter<AlergensAdapter.ViewHolder>{
+public class AlergensAdapter extends RecyclerView.Adapter<AlergensAdapter.ViewHolder> {
 
-    ArrayList<String> list;
+    ArrayList<String> alergensNamesList;
+    ArrayList<String> substancesNamesList;
+    WeakReference<Context> mCtx;
+    boolean added;
+
+    public AlergensAdapter(Context context, ArrayList<String> alergensNamesList, ArrayList<String> substanceNamesList) {
+        this.alergensNamesList = alergensNamesList;
+        mCtx = new WeakReference<Context>(context);
+        this.substancesNamesList = substancesNamesList;
+        added = true;
+    }
+
+    public void add(String name) {
+        alergensNamesList.add(name);
+        notifyDataSetChanged();
+        added = true;
+    }
+
+    public void delete(int index) {
+        alergensNamesList.remove(index);
+        notifyDataSetChanged();
+    }
 
     @Override
     public AlergensAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_alergen, null);
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView, alergensNamesList, this);
         return null;
     }
 
     @Override
     public void onBindViewHolder(AlergensAdapter.ViewHolder holder, int position) {
+        holder.txtView.setText(alergensNamesList.get(position));
+        ArrayAdapter<String> tipsAdapter = new ArrayAdapter<String>(mCtx.get(), R.layout.item_tip, substancesNamesList);
+        holder.txtView.setAdapter(tipsAdapter);
+
+        if (position == getItemCount() - 1 && added == true) {
+            holder.txtView.requestFocus();
+            added = false;
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return alergensNamesList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+        public AutoCompleteTextView txtView;
+        public ImageView imgView;
+
+        public ViewHolder(View itemLayoutView, final ArrayList<String> itemsData, final AlergensAdapter adapter) {
+            super(itemLayoutView);
+            txtView = (AutoCompleteTextView) itemLayoutView.findViewById(R.id.item_alergen_name);
+            imgView = (ImageView) itemLayoutView.findViewById(R.id.item_alergen_delete);
+
+            txtView.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    itemsData.set(getAdapterPosition(), s.toString());
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+            imgView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapter.delete(getAdapterPosition());
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/liutova/avocare/view/fragment/AlergensFragment.java
+++ app/src/main/java/com/liutova/avocare/view/fragment/AlergensFragment.java
@@ -10,36 +10,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.liutova.avocare.AvocareApplication;
 import com.liutova.avocare.R;
 import com.liutova.avocare.helper.Helper;
+import com.liutova.avocare.listener.TypeCompositionFragmentListener;
+import com.liutova.avocare.model.MbAlergens;
 import com.liutova.avocare.network.AsyncTaskTypeComposition;
-import com.liutova.avocare.view.adapter.TypeCompositionAdapter;
+import com.liutova.avocare.view.adapter.AlergensAdapter;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
 
 /**
  * Created by liutoole on 4/18/16.
  */
-public class AlergensFragment extends BaseFragment {
+public class AlergensFragment extends BaseFragment implements TypeCompositionFragmentListener {
+
+    AlergensAdapter adapter;
+    ArrayList<String> substanceNamesList;
 
     @Bind(R.id.alergens_listview)
     RecyclerView alergensView;
 
-    @Override
-    protected int getLayout() {
-        return R.layout.fragment_alergens;
-    }
-
     public static AlergensFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         AlergensFragment fragment = new AlergensFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_alergens;
     }
 
     @Nullable
@@ -47,10 +56,23 @@ public class AlergensFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, container, savedInstanceState);
 
+        substanceNamesList = new ArrayList<String>();
+        String languageID = getBaseActivity().getSharedPreferences("preferences", Context.MODE_PRIVATE).getString("LanguageId", "");
+        AsyncTaskTypeComposition task = new AsyncTaskTypeComposition(languageID, this);
+        task.execute();
+
         ArrayList<String> alergensList = new ArrayList<String>();
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(AvocareApplication.getAppContext()).build();
+        Realm realm = Realm.getInstance(realmConfig);
+        RealmResults<MbAlergens> results = realm.where(MbAlergens.class).findAllSorted("name");
+
+        ArrayList<String> adapterList = new ArrayList<String>();
+        for (MbAlergens item : results) {
+            adapterList.add(item.getName());
+        }
 
         alergensView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new TypeCompositionAdapter(getBaseActivity(), alergensList);
+        adapter = new AlergensAdapter(alergensList);
         alergensView.setAdapter(adapter);
         alergensView.setItemAnimator(new DefaultItemAnimator());
 
@@ -59,6 +81,14 @@ public class AlergensFragment extends BaseFragment {
 
     @OnClick(R.id.add_alergen_btn)
     public void onAddAlergen(){
+        adapter.add("");
+        Helper.showKeyboard(getBaseActivity());
+    }
 
+    @Override
+    public void onGetResults(ArrayList<String> list) {
+        for (String name : list) {
+            substanceNamesList.add(name);
+        }
     }
 }
